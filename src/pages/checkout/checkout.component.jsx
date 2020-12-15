8658ae679ad24159f207a28aea7d13e5ec661605
hiep <hiep@gmail.com>
2020-12-16 06:53:19 +0700

    add checkout-item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,8 +4,37 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
+import CustomButton from '../../components/custom-button/custom-button.component';
+
 import './checkout.styles.scss';
 
+import axios from 'axios';
+
+const test = () => {
+    axios({
+        url: 'orders',
+        method: 'post',
+        data: {
+            "userId": "5fd73c3865e394467846d889",
+            "totalPrice": 200,
+            "details": [
+                {
+                    "productId": "5fd7051ec3649a48983a63a6",
+                    "quantity": 20
+                },
+                {
+                    "productId": "5fd70992be1c344600db7337",
+                    "quantity": 19
+                }
+            ]
+        }
+    }).then(res => {
+        alert('succeed!')
+    }).catch(error => {
+        // console.log('error: ', JSON.parse(error));
+        alert('There was an error.');
+    });
+}
 const CheckoutPage = ({ cartItems, cartTotal }) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
@@ -29,6 +58,9 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
             cartItems.map((item) => item.name)
         }
         <div className='total'> TOTAL: ${cartTotal} </div>
+        <CustomButton onClick={
+            test
+        } > Test API </CustomButton>
     </div>
 );
 
